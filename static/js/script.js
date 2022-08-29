508704a26f8543644ec08b8567682e3df90d10b4
Faris Dhoot <farisgjd@outlook.com>
2022-08-29 18:00:17 +0000

    Style header logo and add custom js script to allow toggling of drop down lists

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+const userProfileIcon = document.getElementById('profile-icon');
+const userProfileDropDown = document.querySelector('.user-profile-drop-down-menu'); 
+
+
+console.log(userProfileIcon)
+console.log(userProfileDropDown)
+
+userProfileIcon.addEventListener("click", function(){
+    userProfileDropDown.classList.toggle("active"); 
+})
+
