a2e59ea62911fb7c04271603646f7df14fca7495
Faris Dhoot <farisgjd@outlook.com>
2022-08-29 18:51:31 +0000

    Style layout of header and add media url to list of urls in skincared urls.py directory

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,6 @@
+// User Profile Dropdown Toggle 
 const userProfileIcon = document.getElementById('profile-icon');
 const userProfileDropDown = document.querySelector('.user-profile-drop-down-menu'); 
-
-
-console.log(userProfileIcon)
-console.log(userProfileDropDown)
 
 userProfileIcon.addEventListener("click", function(){
     userProfileDropDown.classList.toggle("profile-active"); 
