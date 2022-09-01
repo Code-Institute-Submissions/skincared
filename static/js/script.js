087077f8d101f7dd4a12dfc985a3fa458f1590a2
Faris Dhoot <farisgjd@outlook.com>
2022-09-01 04:20:46 +0000

    Create footer

--- static/js/script.js
+++ static/js/script.js
@@ -6,13 +6,16 @@ userProfileIcon.addEventListener("click", function(){
     userProfileDropDown.classList.toggle("profile-active"); 
 })
 
-const navHeading = document.querySelectorAll('.nav-heading'); 
-const navDropDown = document.querySelectorAll('.nav-drop-down'); 
+// const navHeading = document.querySelectorAll('.nav-heading'); 
+// const navDropDown = document.querySelectorAll('.nav-drop-down'); 
 
-console.log(navHeading)
-console.log(navDropDown)
+// console.log(navHeading)
+// console.log(navDropDown)
 
-navHeading[0].addEventListener("click", function(){
-    navDropDown[0].classList.toggle("nav-active");
-} )
-// navHeading.forEach(function)
+// for (i = 0; i < navHeading.length; i++) {
+//     navHeading[i].addEventListener("click", function() {
+//         for (i = 0; i < navDropDown.length; i++) {
+//             this[i].classList.toggle("nav-active")
+//         }
+//     })
+// }
