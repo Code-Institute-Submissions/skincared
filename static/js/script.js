191b7ae865086122febd0a617121778aba155257
Faris Dhoot <farisgjd@outlook.com>
2022-09-01 22:19:24 +0000

    Create js for hamburger menu navigation toggle

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,19 @@
-// User Profile Dropdown Toggle 
+// Main Header User Profile Dropdown 
 const userProfileIcon = document.getElementById('profile-icon');
 const userProfileDropDown = document.querySelector('.user-profile-drop-down-menu'); 
 
 userProfileIcon.addEventListener("click", function(){
     userProfileDropDown.classList.toggle("profile-active"); 
+})
+
+// Mobile Header Dropdowns 
+const hamburger = document.querySelector(".burger-menu"); 
+const navMenu = document.querySelector(".test"); 
+
+console.log(hamburger)
+
+console.log(navMenu)
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");  
 })
