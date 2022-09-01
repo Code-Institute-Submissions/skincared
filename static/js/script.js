411b4cc7e88d2f116a50bab8b210343f35054b66
Faris Dhoot <farisgjd@outlook.com>
2022-09-01 03:57:56 +0000

    Test out navigation js

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,13 @@ userProfileIcon.addEventListener("click", function(){
     userProfileDropDown.classList.toggle("profile-active"); 
 })
 
+const navHeading = document.querySelectorAll('.nav-heading'); 
+const navDropDown = document.querySelectorAll('.nav-drop-down'); 
+
+console.log(navHeading)
+console.log(navDropDown)
+
+navHeading[0].addEventListener("click", function(){
+    navDropDown[0].classList.toggle("nav-active");
+} )
+// navHeading.forEach(function)
