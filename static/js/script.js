a8b3dd779dafe38b499734e3817f39efbb11937c
Faris Dhoot <farisgjd@outlook.com>
2022-09-01 17:45:15 +0000

    Add css to display navigation dropdown menu and delete intended js for this functionality

--- static/js/script.js
+++ static/js/script.js
@@ -5,17 +5,3 @@ const userProfileDropDown = document.querySelector('.user-profile-drop-down-menu
 userProfileIcon.addEventListener("click", function(){
     userProfileDropDown.classList.toggle("profile-active"); 
 })
-
-// const navHeading = document.querySelectorAll('.nav-heading'); 
-// const navDropDown = document.querySelectorAll('.nav-drop-down'); 
-
-// console.log(navHeading)
-// console.log(navDropDown)
-
-// for (i = 0; i < navHeading.length; i++) {
-//     navHeading[i].addEventListener("click", function() {
-//         for (i = 0; i < navDropDown.length; i++) {
-//             this[i].classList.toggle("nav-active")
-//         }
-//     })
-// }
