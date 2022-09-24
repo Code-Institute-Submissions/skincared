f21a9d88befdc8f47dc1ae53fbc0b5074a8fca07
Faris Dhoot <farisgjd@outlook.com>
2022-09-24 03:59:45 +0000

    create brands.html page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Brand
 
-# Create your views here.
+
+def brands(request):
+    """ Renders all brands, sorting and serach queries """
+
+    brands = Brand.objects.all()
+
+    context = {
+        'brands': brands, 
+    }
+
+    return render(request, 'brands/brands.html', context)
